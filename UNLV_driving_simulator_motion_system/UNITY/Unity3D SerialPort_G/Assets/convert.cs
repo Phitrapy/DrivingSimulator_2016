d1ee03d3d7f710de137e2e8bd32a5140ef7c8ee7
Clément Begnaud <clement.begnaud@gmail.com>
2016-08-01 15:24:15 -0700

    clean 3

---
+++ UNLV_driving_simulator_motion_system/UNITY/Unity3D SerialPort_G/Assets/convert.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class convert : MonoBehaviour {
+
+		public int decValue = 800;
+	// Use this for initialization
+	void Start () {
+				string hexValue = decValue.ToString("X");
+				//Convert.ToInt64(hexValue, 16);
+				//string.format("{0:x}", decValue);
+				Debug.Log("decValue -> "+ hexValue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
